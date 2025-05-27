0d5f05b263d1795e0e618ce5360504ebce687ba4
Presley Lopes <presley.lopes.pl@gmail.com>
2025-05-27 12:46:05 +0530

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,1 +1,14 @@
+# versions.tf
+terraform {
+  required_version = ">= 1.0.0" # Specify your desired Terraform CLI version
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0" # Specify your desired AWS provider version
+    }
+  }
+}
 
+provider "aws" {
+  region = var.aws_region # Use the AWS region defined in variables.tf
+}
