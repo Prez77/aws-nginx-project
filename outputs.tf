15aff46f9c9ed548e46d79b47807eda6f8c11ccf
Presley Lopes <presley.lopes.pl@gmail.com>
2025-05-27 12:46:59 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,10 @@
+# outputs.tf
+output "nginx_public_ip" {
+  description = "The public IP address of the Nginx web server"
+  value       = aws_instance.nginx_web_server.public_ip
+}
 
+output "nginx_public_dns" {
+  description = "The public DNS name of the Nginx web server"
+  value       = aws_instance.nginx_web_server.public_dns
+}
